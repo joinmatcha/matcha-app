7a6c3a20bdd56852c7ab8cc624df29b5188580f5
cherel_e <cherel_e@etna-alternance.net>
2025-07-03 17:51:52 +0200

    Add React Native Paper

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import "../global.css"
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native';
+import { DefaultTheme, PaperProvider } from 'react-native-paper';
+import '../global.css';
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '',
+    secondary: 'yellow',
+  },
+};
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-green-white">
-      <Text className="text-xl font-bold text-green-500 ">
-        Welcome to Matcha!
-      </Text>
-    </View>
+    <PaperProvider theme={theme}>
+      <View className="flex-1 items-center justify-center bg-green-white">
+        <Text className="text-xl font-bold" style={{color: theme.colors.primary}}>
+          Welcome to Matcha!
+        </Text>
+      </View>
+    </PaperProvider>
   );
 }
